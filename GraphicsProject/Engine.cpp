6729429f7a0205bf10ccc13d71fbb1415d847ca4
Lucas <s208055>
2021-09-28 13:43:16 -0500

    move main functions to the engine

--- GraphicsProject/Engine.cpp
+++ GraphicsProject/Engine.cpp
@@ -1,12 +1,17 @@
 #include "Engine.h"
 #include "GLFW/glfw3.h"
+#include "gl_core_4_4.h"
+#include <iostream>
 Engine::Engine()
 {
+	//create glfw window
+	m_window = glfwCreateWindow(1280, 720, "Graphics Engine", nullptr, nullptr);
 }
 
 
 Engine::~Engine()
 {
+	delete m_window;
 }
 
 int Engine::run()
@@ -16,7 +21,7 @@ int Engine::run()
 		return exitCode;
 
 	while (getGameOver()) {
-		update();
+		exitCode = update();
 	}
 
 	if (exitCode = end() != 0)
@@ -27,16 +32,45 @@ int Engine::run()
 
 int Engine::start()
 {
+	if (glfwInit() == GLFW_FALSE) {
+		return -1;
+	}
+
+	//if window wasm't sucefully created end application
+	if (!m_window) {
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(m_window);
+
+	//load openGL
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+		glfwDestroyWindow(m_window);
+		glfwTerminate();
+		return -3;
+	}
+
+	//print openGL version to console
+	printf("OpenGL version: %i.%i\n", ogl_GetMajorVersion(), ogl_GetMinorVersion());
+
 	return 0;
 }
 
 int Engine::update()
 {
+	glfwSwapBuffers(m_window);
+	glfwPollEvents();
+
 	return 0;
 }
 
 int Engine::end()
 {
+	//end application
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
+
 	return 0;
 }
 
--- GraphicsProject/Main.cpp
+++ GraphicsProject/Main.cpp
@@ -1,41 +1,12 @@
-#include "gl_core_4_4.h"
-#include "GLFW/glfw3.h"
-#include <iostream>
+#include "Engine.h"
 int main() {
+	int exitCode = 0;
 
-	if (glfwInit() == GLFW_FALSE) {
-		return -1;
-	}
-	//create glfw window
-	GLFWwindow* window = glfwCreateWindow(1280, 720,"Graphics Engine",nullptr,nullptr);
-	//if window wasm't sucefully created end application
-	if (!window) {
-		glfwTerminate();
-		return -2;
-	}
+	Engine* game = new Engine();
 
-	glfwMakeContextCurrent(window);
+	exitCode = game->run();
 
-	//load openGL
-	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
-	
-		glfwDestroyWindow(window);
-		glfwTerminate();
-		return -3;
-	}
+	delete game;
 
-	//print openGL version to console
-	printf("OpenGL version: %i.%i\n", ogl_GetMajorVersion(), ogl_GetMinorVersion());
-
-	while (!glfwWindowShouldClose(window) 
-		&& glfwGetKey(window,GLFW_KEY_ESCAPE) != GLFW_PRESS) {
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-	}
-
-	//end application
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	return 0;
+	return exitCode;
 }
