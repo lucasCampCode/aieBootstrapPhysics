e88a71948f6dd4fb21d4e45c45810ab3f4ad5494
Lucas <s208055>
2021-09-28 12:22:32 -0500

    added OpenGL and include OpenGL dependencies

--- GraphicsProject/Main.cpp
+++ GraphicsProject/Main.cpp
@@ -1,19 +1,35 @@
 #include "gl_core_4_4.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
 int main() {
 
-	if (!glfwInit()) {
+	if (glfwInit() == GLFW_FALSE) {
 		return -1;
 	}
+	//create glfw window
 	GLFWwindow* window = glfwCreateWindow(1280, 720,"Graphics Engine",nullptr,nullptr);
+	//if window wasm't sucefully created end application
 	if (!window) {
 		glfwTerminate();
 		return -2;
 	}
+
 	glfwMakeContextCurrent(window);
 
-	glfwDestroyWindow(window);
+	//load openGL
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+	
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
 
+	//print openGL version to console
+
+	printf("OpenGL version: %i.%i\n", ogl_GetMajorVersion(), ogl_GetMinorVersion());
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
+	//end application
 	return 0;
 }
